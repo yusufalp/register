4c61c566fefcdf2d36cd4a46f3b7694a31fcbe67
Yusuf Alp <alpyusuf007@gmail.com>
2021-03-29 15:58:35 -0700

    add the Register component and an image

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Register from '../Register/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="register-main">
+        <div className="register-main-content">
+          <Register />
+        </div>
+        <div className="register-main-image">
+          <img src="https://usersnap.com/blog/wp-content/uploads/2017/04/web-design-background.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
