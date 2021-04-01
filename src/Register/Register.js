b6bfe948823824eab374e5fbfabca72ae72f1e82
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-01 14:15:59 -0700

    place the form in a div that will be displayed as a modal

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -3,29 +3,31 @@ import './Register.css';
 
 const Register = () => {
   return (
-    <form action="">
-      <div className="form-input-box">
-        <label for="firstName">First Name</label>
-        <input type="text" name="firstName" id="firstName" required />
-      </div>
-      <div className="form-input-box">
-        <label for="lastName">Last Name</label>
-        <input type="text" name="lastName" id="lastName" required />
-      </div>
-      <div className="form-input-box">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" required />
-      </div>
-      <div className="form-input-box">
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" required />
-      </div>
-      <div className="form-input-box">
-        <label for="passwordCheck">Re-type password</label>
-        <input type="password" name="passwordCheck" id="passwordCheck" required />
-      </div>
-      <button type="submit">Register</button>
-    </form>
+    <div className="modal">
+      <form action="" className="modal-content">
+        <div className="form-input-box">
+          <label for="firstName">First Name</label>
+          <input type="text" name="firstName" id="firstName" required autoFocus />
+        </div>
+        <div className="form-input-box">
+          <label for="lastName">Last Name</label>
+          <input type="text" name="lastName" id="lastName" required />
+        </div>
+        <div className="form-input-box">
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" required />
+        </div>
+        <div className="form-input-box">
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password" required />
+        </div>
+        <div className="form-input-box">
+          <label for="passwordCheck">Re-type password</label>
+          <input type="password" name="passwordCheck" id="passwordCheck" required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
   );
 }
 
