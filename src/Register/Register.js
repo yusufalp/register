6d2edfa23a2d92c078692e7064fd9ed1b22bffa4
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-01 13:56:44 -0700

    add a registration modal

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -5,16 +5,28 @@ const Register = () => {
   return (
     <form action="">
       <div className="form-input-box">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" required autoFocus />
+        <label for="firstName">First Name</label>
+        <input type="text" name="firstName" id="firstName" required />
+      </div>
+      <div className="form-input-box">
+        <label for="lastName">Last Name</label>
+        <input type="text" name="lastName" id="lastName" required />
+      </div>
+      <div className="form-input-box">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" required />
       </div>
       <div className="form-input-box">
         <label for="password">Password</label>
         <input type="password" name="password" id="password" required />
       </div>
-      <button type="submit">Sign On</button>
+      <div className="form-input-box">
+        <label for="passwordCheck">Re-type password</label>
+        <input type="password" name="passwordCheck" id="passwordCheck" required />
+      </div>
+      <button type="submit">Register</button>
     </form>
-  )
+  );
 }
 
 export default Register;
