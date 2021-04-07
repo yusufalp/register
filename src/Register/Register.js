26391c0c8ec2411ccf2c4c6987bf1d5e62ad71e7
Yusuf Alp <alpyusuf007@gmail.com>
2021-04-07 15:19:58 -0700

    add a password requirements check control, atleast 8 characters, one capital leter and one number

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,28 +1,68 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './Register.css';
 
-const Register = () => {
+const Register = (props) => {
+  const div = useRef();
+
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+
+  const handleClickOutside = (e) => {
+    if (div.current === e.target) {
+      props.onClickOutside();
+    }
+  }
+
+  const handleClickIcon = () => {
+    props.onClickOutside();
+  }
+
+  const onChangeRegisterPassword = (e) => {
+    let value = e.target.value;
+
+    let checkUpperCase = /[A-Z]/g;
+    let checkNumber = /[0-9]/g;
+    let letterResult = checkUpperCase.test(value);
+    let numberResult = checkNumber.test(value);
+
+    if (!(letterResult && numberResult)) {
+      setPasswordErrorMessage("Must include capital letter and a number!");
+    } else if (value.length < 8) {
+      setPasswordErrorMessage("At least 8 characters required!");
+    } else {
+      setPasswordErrorMessage("");
+    }
+
+  }
+
   return (
-    <div className="modal">
+    <div ref={div} className="modal" onClick={handleClickOutside}>
       <form action="" className="modal-content">
+        <span className="close-icon" ><i onClick={handleClickIcon} className="fas fa-times"></i></span>
         <div className="form-input-box">
-          <label for="firstName">First Name</label>
+          <label htmlFor="firstName">First Name</label>
           <input type="text" name="firstName" id="firstName" required autoFocus />
         </div>
         <div className="form-input-box">
-          <label for="lastName">Last Name</label>
+          <label htmlFor="lastName">Last Name</label>
           <input type="text" name="lastName" id="lastName" required />
         </div>
         <div className="form-input-box">
-          <label for="email">Email</label>
+          <label htmlFor="email">Email</label>
           <input type="email" name="email" id="email" required />
         </div>
         <div className="form-input-box">
-          <label for="password">Password</label>
-          <input type="password" name="password" id="password" required />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={onChangeRegisterPassword}
+            required
+          />
+          <p className="error-message">{passwordErrorMessage}</p>
         </div>
         <div className="form-input-box">
-          <label for="passwordCheck">Re-type password</label>
+          <label htmlFor="passwordCheck">Re-type password</label>
           <input type="password" name="passwordCheck" id="passwordCheck" required />
         </div>
         <button type="submit">Register</button>
